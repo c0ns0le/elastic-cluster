b9ae82862564e069061d5487242fce6923087d6d
Mats Iremark <mats.x.iremark@collectorbank.se>
2016-02-05 17:23:43 +0100

    Changes

---
+++ install_elastic.sh
@@ -0,0 +1,38 @@
+cd ~
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+sudo aptitude -y install oracle-java8-installer
+
+
+### Download and Install ElasticSearch
+### Check http://www.elasticsearch.org/download/ for latest version of ElasticSearch and replace wget link below
+wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-2.2.0.deb
+sudo dpkg -i elasticsearch-2.2.0.deb
+
+wget https://github.com/iremmats/elastic-cluster/blob/master/elasticsearch_master.yml
+sudo cp elasticsearch_master.yml /etc/elasticsearch/
+
+#enable on bootup
+sudo update-rc.d elasticsearch defaults 95 10
+ 
+### Start ElasticSearch 
+sudo /etc/init.d/elasticsearch start
+
+### Make sure service is running
+curl http://localhost:9200
+
+### Should return something like this:
+# {
+#  "status" : 200,
+#  "name" : "Storm",
+#  "version" : {
+#    "number" : "1.3.1",
+#    "build_hash" : "2de6dc5268c32fb49b205233c138d93aaf772015",
+#    "build_timestamp" : "2014-07-28T14:45:15Z",
+#    "build_snapshot" : false,
+#    "lucene_version" : "4.9"
+#  },
+#  "tagline" : "You Know, for Search"
+#}
